3af0668aa38542f20e58b4e13e5bfa4cc3a9d469
Lucas Possatti <lucas_possatti@hotmail.com>
2017-12-21 01:56:01 -0200

    Primeiro commit

---
+++ data/download_cluttered_mnist.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+CLUTTERED_MNIST_URL="https://github.com/daviddao/spatial-transformer-tensorflow/raw/master/data/mnist_sequence1_sample_5distortions5x5.npz"
+
+wget --continue "$CLUTTERED_MNIST_URL"
